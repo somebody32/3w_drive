a6b335a2803cdc54569f197d9f1a03a52cc2fc6f
Ilia Zayats <somebody32@gmail.com>
2010-05-04 12:44:15 +0400

    added robot visualization

--- lib/3w_ride.rb
+++ lib/3w_ride.rb
@@ -1,18 +1,95 @@
-module Ride
+module UIMethods
+  LEFT_MARGIN = 12
+
+  def self.add_wheel_fields(wheel_no)
+    $app.tagline "#{wheel_no} wheel", :margin_bottom => 5, :margin_top => 15, :padding => 0, :align => 'center'
+
+    $app.inscription 'Angle',    :margin => 0, :margin_left => LEFT_MARGIN
+    $app.edit_line               :margin => 0, :margin_left => LEFT_MARGIN-4, :width => 100
+    $app.inscription 'Momentum', :margin => 0, :margin_left => LEFT_MARGIN
+    $app.edit_line               :margin => 0, :margin_left => LEFT_MARGIN-4, :width => 100
+  end
+end
+
+class Robot
+  attr_reader :x, :y, :wheels
   
-  module UIMethods
-    LEFT_MARGIN = 12
+  
+  def initialize
+    @x, @y = $width-$left + 50, $height/2 - $top - 50
+    @center_x, @center_y = @x + 50, @y + 50
     
-    def self.add_wheel_fields(wheel_no)
-      $app.tagline "#{wheel_no} wheel", :margin_bottom => 5, :margin_top => 15, :padding => 0, :align => 'center'
-      
-      $app.inscription 'Angle',    :margin => 0, :margin_left => LEFT_MARGIN
-      $app.edit_line               :margin => 0, :margin_left => LEFT_MARGIN-4, :width => 100
-      $app.inscription 'Momentum', :margin => 0, :margin_left => LEFT_MARGIN
-      $app.edit_line               :margin => 0, :margin_left => LEFT_MARGIN-4, :width => 100
+    @wheels = []
+    @wheels << Wheel.new(@center_x, @center_y-45, 90)
+    @wheels << Wheel.new(@center_x-20, @center_y+5, 0)
+    @wheels << Wheel.new(@center_x+20, @center_y+5, 0)
+  end
+  
+  def draw
+    $app.fill $app.red
+    $app.oval(@x, @y, 100)
+    $app.fill $app.white
+    @wheels.each {|w| w.draw }
+  end
+  
+end
+
+class Wheel
+  attr_reader :x, :y
+
+	def initialize x, y, angle
+		@x, @y, @angle = x, y, angle
+
+		$app.rotate @angle
+	end
+
+	def draw
+		$app.fill $app.white
+    $app.arrow(@x, @y, 20)
+	end
+end
+
+class Operator
+  
+  def self.new_experiment
+    @robot = Robot.new
+  end
+  
+  def self.update_scene
+    $app.clear do
+      create_scene
+      @robot.draw
     end
   end
   
+  def self.create_scene
+    @controls = $app.stack :width => 0.25 do 
+      $app.background rgb(210, 210, 210), :curve => 20
+
+      %w(First Second Third).each { |i| UIMethods.add_wheel_fields(i) }
+
+      $app.flow :margin_left => 60, :margin_top => 20 do
+        $app.button "Run" do
+
+        end
+      end       
+    end
+
+    @visualization = $app.stack :width => 0.75 do
+      $app.background $app.black, :curve => 12    
+      $app.para "visualization", :stroke => $app.white, :align => "center"
+    end
+
+    @controls.style(STACK_STYLE)
+    @visualization.style(STACK_STYLE)
+
+    $width, $height =  @visualization.width,  @visualization.height 
+
+    $left   = $app.width  - @visualization.width - 5
+    $right  = $left  + @visualization.width
+    $top    = 5
+    $bottom = $app.height - 5
+  end  
 end
 
 
@@ -22,25 +99,9 @@ Shoes.app(:title => '3W Ride', :width => 800, :height => 520, :resizable => fals
   
   $app = self
   
+  Operator.new_experiment
   
-  @controls = stack :width => 0.25 do 
-    background rgb(210, 210, 210), :curve => 20
-    
-    %w(First Second Third).each { |i| Ride::UIMethods.add_wheel_fields(i) }
-    
-    flow :margin_left => 60, :margin_top => 20 do
-      button "Run" do
-        
-      end
-    end       
+  animate(60) do
+    Operator.update_scene
   end
-  
-  @visualization = stack :width => 0.75 do
-    background black, :curve => 12
-    
-    para "visualization", :stroke => white, :align => "center"
-  end
-  
-  @controls.style(STACK_STYLE)
-  @visualization.style(STACK_STYLE)
 end
